4cad8e36c591a8cfb4aacf9f98405b43187733f7
Daniel Spiewak <djspiewak@gmail.com>
2008-11-07 14:09:59 -0600

    Added a proper spec

--- src/test/scala/HashMapSpecs.scala
+++ src/test/scala/HashMapSpecs.scala
@@ -1,11 +1,19 @@
 import org.specs._
+import org.scalacheck._
 
 import com.codecommit.collection.HashMap
 
-object HashMapSpecs extends Specification {
+object HashMapSpecs extends Specification with Scalacheck {
+  import Prop._
+  
   "it" should {
-    "not die" in {
-      (0 until 33).foldLeft(new HashMap[Int, Int]) { (m, n) => m(n) = -n }
+    "store objects" in {
+      val prop = property { src: Map[Any, Any] =>
+        val map = src.foldLeft(new HashMap[Any, Any]) { _ + _ }
+        src forall { case (k, v) => map(k) == v }
+      }
+      
+      prop must pass
     }
   }
 }
