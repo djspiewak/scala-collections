892fc0ea4bcb282ef2e8f16050b5cf42115c0c72
Daniel Spiewak <djspiewak@gmail.com>
2008-11-07 14:33:01 -0600

    Fixed specification

--- src/test/scala/HashMapSpecs.scala
+++ src/test/scala/HashMapSpecs.scala
@@ -7,13 +7,31 @@ object HashMapSpecs extends Specification with Scalacheck {
   import Prop._
   
   "it" should {
-    "store objects" in {
-      val prop = property { src: Map[Any, Any] =>
-        val map = src.foldLeft(new HashMap[Any, Any]) { _ + _ }
-        src forall { case (k, v) => map(k) == v }
+    "store ints" in {
+      val prop = property { src: List[Int] =>
+        val map = src.foldLeft(new HashMap[Int, Int]) { (m, v) => m(v) = -v }
+        src forall { v => map(v) == -v }
       }
       
       prop must pass
     }
+    
+    "store strings" in {
+      val prop = property { src: List[String] =>
+        val map = src.foldLeft(new HashMap[String, Int]) { (m, v) => m(v) = v.length }
+        src forall { v => map(v) == v.length }
+      }
+      
+      prop must pass
+    }
+    
+    
   }
+  
+  /* implicit def arbHashMap[K, V](implicit ak: Arbitrary[List[K]], implicit av: Arbitrary[(K)=>V]): Arbitrary[HashMap[K, V]] = {
+    Arbitrary(for {
+      keys <- ak.arbitrary
+      f <- av.arbitrary
+    } yield keys.foldLeft(new HashMap[K, V]) { (m, k) => m(k) = f(k) })
+  } */
 }
