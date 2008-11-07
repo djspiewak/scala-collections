b8b32aa3c5d90cbb85e4cb07728fc217e4898fc2
Daniel Spiewak <djspiewak@gmail.com>
2008-11-07 17:46:11 -0600

    Modified do_perf script to account for new changes

--- do_perf.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-buildr compile && "C:\Program Files\Java\jdk1.6.0_10\bin\java" -Xmx1024m -Xms256m -server -cp "C:\Program Files\Scala\lib\scala-library.jar;target\classes" VectorPerfTest
